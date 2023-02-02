077b46155d83144ed43890c74b6234e9d8705402
Emanuele213 <vittorius213@gmail.com>
2023-02-02 15:01:28 +0100

    first commit

---
+++ resources/js/front.js
@@ -0,0 +1,12 @@
+const { h } = require('vue');
+
+require('./bootstrap');
+
+// window.Vue = require('vue');
+import Vue from 'vue';
+import App from './App.vue'
+
+new Vue({
+    el: '#root',
+    render: h => h(App),
+})
